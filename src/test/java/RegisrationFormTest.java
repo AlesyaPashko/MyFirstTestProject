0ad47bfb0b4196231bf2ec24052d7d3587478f02
Alesia Pashko <Pashko218@gmail.com>
2023-05-26 12:27:21 +0300

    form test

--- src/test/java/RegisrationFormTest.java
+++ src/test/java/RegisrationFormTest.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -8,6 +10,25 @@ public class RegisrationFormTest {
     public void CheckRegistrationForm(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        WebElement textInputField = driver.findElement(By.id("my-text-id"));
+        textInputField.sendKeys("Банана");
+
+        WebElement passwordField = driver.findElement(By.name("my-password"));
+        passwordField.sendKeys("НуОченьСекретныйПароль");
+
+        WebElement textareaField = driver.findElement(By.xpath("//textarea[@rows=\"3\"]"));
+        textareaField.sendKeys("Once Upon a Time...");
+
+        WebElement dropdownSelect = driver.findElement(By.xpath("//*[@class=\"form-select\"]"));
+        dropdownSelect.click();
+
+        WebElement dropdownSelectOne = driver.findElement(By.xpath("//*[@value=\"1\"]"));
+        dropdownSelectOne.click();
+
+        driver.quit();
+
+
 
     }
 }
