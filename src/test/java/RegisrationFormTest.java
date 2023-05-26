77b926de5ba252dca0521310713aaaf1eb0b0c0c
Alesia Pashko <Pashko218@gmail.com>
2023-05-26 13:01:38 +0300

    add assert

--- src/test/java/RegisrationFormTest.java
+++ src/test/java/RegisrationFormTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class RegisrationFormTest {
@@ -25,6 +26,14 @@ public class RegisrationFormTest {
 
         WebElement dropdownSelectOne = driver.findElement(By.xpath("//*[@value=\"1\"]"));
         dropdownSelectOne.click();
+
+        WebElement sublimitButton = driver.findElement(By.xpath("//*[@class=\"btn btn-outline-primary mt-3\"]"));
+        sublimitButton.click();
+
+        WebElement finishPage = driver.findElement(By.xpath("//*[@class=\"col-12 py-2\"]"));
+
+
+        Assert.assertTrue(finishPage.getText().contains("Received!"));
 
         driver.quit();
 
